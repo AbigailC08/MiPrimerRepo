8f2f21ae7a9cbc5adecb1632f2a25b17f7252f7b
Abigail <cardozogarciaabigail@gmail.com>
2026-09-16 13:30:40 -0600

    agregue el holamundo.py

---
+++ holamundo.py
@@ -0,0 +1,10 @@
+#Este es un comentario 
+print ('este es mi primer programa es python')
+print ('Bienvenidos a Python')
+#manejo de variables en python
+edad=22
+nombre='Abigail'
+lista=[1,2,3,4,5,6,7,8,9]
+print('la variable edad guardada', edad)
+print('la variable nombre guardada', nombre)
+print('la variable lista guardada', lista)
